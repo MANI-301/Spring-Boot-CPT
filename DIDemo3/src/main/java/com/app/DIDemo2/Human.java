be2fe6fced6034e68ff68d05c3789b895800d3f2
Mani <146062828+MANI-301@users.noreply.github.com>
2026-05-06 18:24:30 +0530

    Spring boot

---
+++ DIDemo3/src/main/java/com/app/DIDemo2/Human.java
@@ -0,0 +1,36 @@
+package com.app.DIDemo2;
+
+import org.springframework.beans.factory.annotation.Autowired;
+
+public class Human {
+
+	@Autowired
+	private Heart heart;// dependency in the form of object
+	
+	public Human() {
+	
+	}
+   
+//	@Autowired
+	public Human(Heart heart) {
+		super();
+		System.out.println("Parameter Constructor of Human");
+		
+		this.heart = heart;
+	}
+//	@Autowired
+	public void setHeart(Heart heart) {
+		System.out.println("In Setter Of Heart");
+		this.heart = heart;
+	}
+	
+	public void pumping() {
+		
+		if(heart==null) {
+		System.out.println("Yor are Dead");
+		}
+		else {
+			heart.pump();
+		}
+	}
+}
